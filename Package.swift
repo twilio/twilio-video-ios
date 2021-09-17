e9cea5bb66341a2aa1d90276e240577cadd54bbb
twilio-sdk-build <twilio-sdk-build@twilio.com>
2021-09-17 20:11:18 +0000

    Update for 4.6.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVideo",
-            url: "https://github.com/twilio/twilio-video-ios/releases/download/4.5.0/TwilioVideo.xcframework.zip",
-            checksum: "5428a20133aa829f9d7437704bd1235dd90e8bb96b42a50667d492dde357f068"
+            url: "https://github.com/twilio/twilio-video-ios/releases/download/4.6.0/TwilioVideo.xcframework.zip",
+            checksum: "184a4e26759aa61a3ee28a306b79ade9318d2b36c4bc866705c9ed2fffdff17a"
         )
     ]
 )
