2f8fdc756dc6a4adb1bae08cda54764d4a355d37
Dipankar Das <didas@twilio.com>
2021-10-05 14:31:55 -0700

    try it out

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVideo",
+            exclude: ["iOS-13-Migration-Guide.md"],
             url: "https://github.com/twilio/twilio-video-ios/releases/download/4.6.0/TwilioVideo.xcframework.zip",
             checksum: "184a4e26759aa61a3ee28a306b79ade9318d2b36c4bc866705c9ed2fffdff17a"
         )
