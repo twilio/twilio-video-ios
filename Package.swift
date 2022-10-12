a51cde9b5b1ec92472cb5cf819c24ec4eb4b897b
twilio-sdk-build <twilio-sdk-build@twilio.com>
2022-10-12 22:30:37 +0000

    Update for 5.3.0

--- Package.swift
+++ Package.swift
@@ -3,20 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "TwilioVideo",
+    name: "RELEASE_PACKAGE_NAME",
     platforms: [
         .iOS("12.2")
     ],
     products: [
         .library(
-            name: "TwilioVideo",
-            targets: ["TwilioVideo"]),
+            name: "RELEASE_PACKAGE_NAME",
+            targets: ["RELEASE_PACKAGE_NAME"]),
     ],
     targets: [
         .binaryTarget(
-            name: "TwilioVideo",
-            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.2.1/TwilioVideo.xcframework.zip",
-            checksum: "f38bde50fc021e052d67e2aec2a9abf340b9f3008f613e46c5c4641c2324881a"
-        )
+            name: "RELEASE_PACKAGE_NAME",
+            url: "RELEASE_ARCHIVE_URL",
+            checksum: "RELEASE_CHECKSUM"
+        ),
     ]
 )
