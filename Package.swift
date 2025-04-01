e23ca54fb2f0b4787d1c8ebf8c8a074295b6e2a4
Bobie Chen <bchen@twilio.com>
2025-04-01 11:13:44 -0700

    Update for 0.0.99

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVideo",
-            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.9.0/TwilioVideo.xcframework.zip",
-            checksum: "62c2f0c4886c4d29a72444ad51ae810527cc37effb8f39a22a6045a0585bd22b"
+            url: "https://github.com/twilio/twilio-video-ios/releases/download/0.0.99/TwilioVideo.xcframework.zip",
+            checksum: "67a80698eced4d29dba86759d5ff412b3f3427323d329ec7e9a35845c32627f8"
         ),
     ]
 )
