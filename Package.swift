42bf73df6f24a2edb9883a7e56dcf95ea995c0fd
twilio-sdk-build <twilio-sdk-build@twilio.com>
2025-09-03 18:54:44 +0000

    Update for 5.10.1

--- Package.swift
+++ Package.swift
@@ -13,18 +13,18 @@ let package = Package(
             targets: ["TwilioVideo"]),
         .library(
             name: "TwilioVideo-static",
-            targets: ["TwilioVideo"]),
+            targets: ["TwilioVideo"])
     ],
     targets: [
         .binaryTarget(
             name: "TwilioVideo",
-            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.10.0/TwilioVideo.xcframework.zip",
-            checksum: "5ffc7378e83f0ca93630eb84edae2d5bfa54a8251afa982316ac166db6e4fbb6"
+            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.10.1/TwilioVideo.xcframework.zip",
+            checksum: "cada84190cf33254e3cf1e094a704d6987f7f8c3ec1c04b205aaa89c70a74b17"
         ),
         .binaryTarget(
             name: "TwilioVideo-static",
-            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.10.0/TwilioVideo-static.xcframework.zip",
-            checksum: "386f2ccb50b7a087fd537505900834d7278f8634f5787ea1898d30c494365dc6"
+            url: "https://github.com/twilio/twilio-video-ios/releases/download/5.10.1/TwilioVideo-static.xcframework.zip",
+            checksum: "fb67237d8cea22cf714e3730c3d5fa99ddf98e28ee9e3d88687ebab1fd70fd67"
         )
     ]
 )
